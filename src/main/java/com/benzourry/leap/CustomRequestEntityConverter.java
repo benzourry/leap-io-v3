10f7b8ce73c3f7c16aa20c0c40c3f8635f93668b
blmrazif <R@zen6razen6>
2026-02-06 14:28:19 +0800

    Update langchain4j dependency: 1.9.1 -> 1.10.0
    Lambda: always put _param as member eventhough it is empty

--- src/main/java/com/benzourry/leap/CustomRequestEntityConverter.java
+++ src/main/java/com/benzourry/leap/CustomRequestEntityConverter.java
@@ -1,36 +1,36 @@
-package com.benzourry.leap;
-
-import org.springframework.core.convert.converter.Converter;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.RequestEntity;
-import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequest;
-import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequestEntityConverter;
-import org.springframework.util.MultiValueMap;
-
-//import com.fasterxml.jackson.databind.util.Converter;
-
-public class CustomRequestEntityConverter implements Converter<OAuth2AuthorizationCodeGrantRequest, RequestEntity<?>> {
-
-    private OAuth2AuthorizationCodeGrantRequestEntityConverter defaultConverter;
-
-    public CustomRequestEntityConverter() {
-        defaultConverter = new OAuth2AuthorizationCodeGrantRequestEntityConverter();
-    }
-
-    @Override
-    public RequestEntity<?> convert(OAuth2AuthorizationCodeGrantRequest req) {
-        RequestEntity<?> entity = defaultConverter.convert(req);
-        MultiValueMap<String, String> params =  (MultiValueMap<String, String>) entity.getBody();
-        String url = params.getFirst("redirect_uri");
-        if(url.contains("facebook")||url.contains("twitter")||url.contains("azuread")){
-            url = url.replace("http://", "https://");
-        }
-        params.set("redirect_uri", url);
-
-        HttpMethod method = url.contains("sarawakid")?HttpMethod.POST: entity.getMethod();
-        //log.info("Callback Request Parameters: "+params.toSingleValueMap().toString());
-        return new RequestEntity<>(params, entity.getHeaders(),
-                method, entity.getUrl());
-    }
-
-}
+//package com.benzourry.leap;
+//
+//import org.springframework.core.convert.converter.Converter;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.http.RequestEntity;
+//import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequest;
+//import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequestEntityConverter;
+//import org.springframework.util.MultiValueMap;
+//
+////import com.fasterxml.jackson.databind.util.Converter;
+//
+//public class CustomRequestEntityConverter implements Converter<OAuth2AuthorizationCodeGrantRequest, RequestEntity<?>> {
+//
+//    private OAuth2AuthorizationCodeGrantRequestEntityConverter defaultConverter;
+//
+//    public CustomRequestEntityConverter() {
+//        defaultConverter = new OAuth2AuthorizationCodeGrantRequestEntityConverter();
+//    }
+//
+//    @Override
+//    public RequestEntity<?> convert(OAuth2AuthorizationCodeGrantRequest req) {
+//        RequestEntity<?> entity = defaultConverter.convert(req);
+//        MultiValueMap<String, String> params =  (MultiValueMap<String, String>) entity.getBody();
+//        String url = params.getFirst("redirect_uri");
+//        if(url.contains("facebook")||url.contains("twitter")||url.contains("azuread")){
+//            url = url.replace("http://", "https://");
+//        }
+//        params.set("redirect_uri", url);
+//
+//        HttpMethod method = url.contains("sarawakid")?HttpMethod.POST: entity.getMethod();
+//        //log.info("Callback Request Parameters: "+params.toSingleValueMap().toString());
+//        return new RequestEntity<>(params, entity.getHeaders(),
+//                method, entity.getUrl());
+//    }
+//
+//}
